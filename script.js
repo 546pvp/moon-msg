ef0d0fe4a72888774e7eccc3b532f41188c1bebd
546pvp <79662987+546pvp@users.noreply.github.com>
2022-10-24 18:57:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ username = username.charAt(0).toUpperCase() + username.slice(1)
 var msginput = document.getElementById("msginput")
 var msglist = document.getElementById("msglist")
 var msglistbox = document.getElementById("msglistbox")
-var base = "https://database-5d763-default-rtdb.firebaseio.com/"
+var base = "YOUR DATABASE URL"
 f1 = new Firebase(base + "/messages")
 
 
